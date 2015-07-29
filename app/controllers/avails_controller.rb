0e23d8c0c0e104560cb8e0f49d24cd6f44e3920b
Zlerp <zhpdeals@gmail.com>
2015-07-29 16:32:35 -0400

    fix availability controller

--- app/controllers/avails_controller.rb
+++
@@ -1,2 +0,0 @@
-class AvailsController < ApplicationController
-end
--- test/controllers/availabilities_controller_test.rb
+++ test/controllers/availabilities_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AvailsControllerTest < ActionController::TestCase
+class AvailabilitiesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
