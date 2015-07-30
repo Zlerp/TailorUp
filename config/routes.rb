afb43d039915b21799b535e1c44621912c3d42f3
Zlerp <zhpdeals@gmail.com>
2015-07-29 23:53:36 -0400

    Working Devise Account Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'tailors#index'
+
+  devise_for :tailor
+  resources :tailors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
